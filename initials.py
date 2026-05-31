61113994034ef08f8673e34006762e1b0ac2de82
Trinityahiakpatsi-cpu <Trinityahiakpatsi@icloud.com>
2026-05-31 19:08:23 +0000

    assignment 1 completed

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
 
-def main():
-  pass #code goes here
 
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Trinity Ahiakpatsi
+# STUDENT ID: 2425400673
+# Email: Trinityahiakpatsi@icloud.com
+# Grader: Augustus
+#
+# On my honor, Trinity Ahiakpatsi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+
+def main():
+    # Print my initials in large stylized letters.
+
+    print()
+    print("...TAT")
+    print()
+
+    print("...TTTTTTTTTTTT....AAAAAAAAAAAA....TTTTTTTTTTTT....")
+    print("......TTTT........AAAA....AAAA......TTTT..........")
+    print("......TTTT........AAAA....AAAA......TTTT..........")
+    print("......TTTT........AAAA....AAAA......TTTT..........")
+    print("......TTTT........AAAAAAAAAAAA......TTTT..........")
+    print("......TTTT........AAAAAAAAAAAA......TTTT..........")
+    print("......TTTT........AAAA....AAAA......TTTT..........")
+    print("......TTTT........AAAA....AAAA......TTTT..........")
+    print("......TTTT........AAAA....AAAA......TTTT..........")
+    print("......TTTT..**....AAAA....AAAA..**..TTTT..**......")
+
+    print()
 
 
 main()
